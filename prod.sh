71bfe149170f7697e2ae38784c1fd3358f3b6e98
Allan Ebdrup <allan@878.dk>
2016-02-04 13:17:31 +0100

    use exit instead of return

--- prod.sh
+++ prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 branch_name=${1// /_}
-git checkout -b "${branch_name}" || return $?
+git checkout -b "${branch_name}" || exit $?
 git commit -am "${branch_name}" || echo "ignoring commit problem"
-git push --set-upstream origin "${branch_name}" || return $?
-hub pull-request -m "${branch_name}" || return $?
-tcmerge "${branch_name}" || return $?
+git push --set-upstream origin "${branch_name}" || exit $?
+hub pull-request -m "${branch_name}" || exit $?
+tcmerge "${branch_name}" || exit $?
