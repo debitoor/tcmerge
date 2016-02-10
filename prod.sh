eadd709c5101befadfd5ef07109fc2b965a6be72
Allan Ebdrup <allan@878.dk>
2016-02-10 10:59:59 +0100

    Also add new files created or files deleted in prod command

--- prod.sh
+++ prod.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 branch_name=${1// /_}
 git checkout -b "${branch_name}" || exit $?
-git commit -am "${branch_name}" || echo "ignoring commit problem"
+git add . -A
+git commit -m "${branch_name}" || echo "ignoring commit problem"
 git push --set-upstream origin "${branch_name}" || exit $?
 hub pull-request -m "${branch_name}" || exit $?
 tcmerge "${branch_name}" || exit $?
