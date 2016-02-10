3980ee3271e37ff03567018a377fafd2047a9fd3
Allan Ebdrup <allan@878.dk>
2016-02-10 13:43:15 +0100

    don't replace spaces in PR description

--- prod.sh
+++ prod.sh
@@ -4,5 +4,5 @@ git checkout -b "${branch_name}" || exit $?
 git add . -A
 git commit -m "${branch_name}" || echo "ignoring commit problem"
 git push --set-upstream origin "${branch_name}" || exit $?
-hub pull-request -m "${branch_name}" || exit $?
+hub pull-request -m "$1" || exit $?
 tcmerge "${branch_name}" || exit $?
