fcd363c081a3b0fdd1efd500db6474f3a18f41c8
Allan Ebdrup <allan@878.dk>
2016-08-12 14:52:55 +0200

    fix prod and commit commands

--- prod.sh
+++ prod.sh
@@ -3,7 +3,7 @@
 commitAndDeploy(){
 	git add . -A
 	git commit -m "${branch_name}" || echo "ignoring commit problem"
-	if [ "$1" = 'do-pull-request' ]
+	if [ "$2" = 'do-pull-request' ]
 	then
 	    git push --set-upstream origin "${branch_name}" || exit $?
 		hub pull-request -m "$1" || exit $?
@@ -18,12 +18,12 @@ branch_name=${1// /_}
 if [ "$current_branch" = 'master' ]
 then
 	git checkout -b "${branch_name}" || exit $?
-	commitAndDeploy do-pull-request
+	commitAndDeploy $1 do-pull-request
 else
 	if [ "$current_branch" = "$branch_name" ]
 	then
 		echo "Already on the branch ${branch_name}"
-		commitAndDeploy
+		commitAndDeploy $1
 	else
 		echo "Not on master branch and not on branch ${branch_name}, exiting with no action"
 	fi
